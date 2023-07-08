24a7527c9b96376e931a756041c39e0e6656afc3
Francesco Ciocchetti <primeroznl@gmail.com>
2023-07-08 12:45:07 +0200

    Initial work on mqtt pipeline controller

---
+++ mqtt-pipeline-controller/api/types.go
@@ -0,0 +1,65 @@
+/*
+ *
+ * Copyright 2022. Metacontroller authors.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * /
+ */
+
+// +groupName=primeroz.xyz
+package v1
+
+import (
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// MqttPublisher
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=mqttpublishers,scope=Namespaced
+type MqttPublisher struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata"`
+
+	Spec   MqttPublisherSpec   `json:"spec"`
+	Status MqttPublisherStatus `json:"status,omitempty"`
+}
+
+type MqttPublisherSpec struct {
+	topcName string `json:"topicName"`
+}
+
+type MqttPublisherStatus struct {
+	observedGeneration int    `json:"observedGeneration,omitempty"`
+	ready              string `json:"ready,omitempty"`
+}
+
+// MqttSubscriber
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=mqttsubscribers,scope=Namespaced
+type MqttSubscriber struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata"`
+
+	Spec   MqttSubscriberSpec   `json:"spec"`
+	Status MqttSubscriberStatus `json:"status,omitempty"`
+}
+
+type MqttSubscriberSpec struct {
+	topcName string `json:"topicName"`
+}
+
+type MqttSubscriberStatus struct {
+	observedGeneration int    `json:"observedGeneration,omitempty"`
+	ready              string `json:"ready,omitempty"`
+}
---
+++ mqtt-pipeline-controller/jsonnet/sync-mqtt-publisher.jsonnet
@@ -0,0 +1,25 @@
+local k = import 'vendor/github.com/jsonnet-libs/k8s-libsonnet/1.26/main.libsonnet';
+
+local secret = k.core.v1.secret;
+
+function(request) {
+  local parent = request.parent,
+  local childrenSecret = std.get(request.children, 'Secret.v1'),
+
+  // Create and return a random secret
+  resyncAfterSeconds: if (std.objectHas(childrenSecret, 'metadata') && std.objectHas(childrenSecret.metadata, 'resourceVersion')) then 30.0 else 1800.0,
+  status: {
+    observedGeneration: std.get(parent.metadata, 'generation', 0),
+    ready: if (std.objectHas(childrenSecret, 'metadata') && std.objectHas(childrenSecret.metadata, 'resourceVersion')) then 'true' else 'false',
+  },
+  children: [
+    secret.new(
+      parent.spec.secretName,
+      {
+        host: 'test.mosquitto.net',
+        port: '1883',
+        topic: request.parent.spec.topicName,
+      }
+    ),
+  ],
+}
